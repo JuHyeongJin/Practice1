a9f7e6953f8a686eba8d9a1f87ff9cb60f7ddcd4
JuHyeongJin <wngudwlswkd@gmail.com>
2022-05-26 20:39:26 +0900

    update

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ let computerNum = 0;
 let playbutton = document.getElementById("playbutton");
 let userinput = document.getElementById("userinput");
 let resultarea = document.getElementById("resultarea");
+let resetbutton = document.getElementById("reset-button");
 
 playbutton.addEventListener("click",play);
+resetbutton.addEventListener("click",reset);
 
 function picRandomNum(){
     computerNum = Math.floor(100 * Math.random()) + 1;
@@ -27,6 +29,15 @@ function play(){
     }else{
         resultarea.textContent = "정답!";
     }
+}
+
+function reset(){
+    //user input 창 정리
+    userinput.value = "";
+    //새로운 번호 생성
+    picRandomNum();
+
+    resultarea.textContent = "결과값이 여기 나옵니다";
 }
 
 
